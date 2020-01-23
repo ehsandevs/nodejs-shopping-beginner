7d15f4cdd38ab3300be135401c9aa617ca790769
Amir.E Haji Hussaini <niamileo@gmail.com>
2020-01-23 16:40:28 +0330

    seperate routing codes to routes folder

--- app.js
+++ app.js
@@ -3,22 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 // Parse incoming text from forms
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"></input><button type="submit">Add Product</button></form>');
-});
-
-// only for POST methods
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express.js</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes)
 
 // for running server on localhost:3000
 app.listen(3000);
