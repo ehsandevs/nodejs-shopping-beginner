06ec5ae4320ac848c5b51f621bb94c6d77fe4e90
Amir.E Haji Hussaini <niamileo@gmail.com>
2020-01-23 11:28:04 +0330

    to the express.js

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes.handler);
+const app = express();
 
-console.log(routes.sometext);
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log("in the middleware!");
+    next(); // Allow the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log("in another middleware!");
+    res.send('<h1>Hello from Express.js</h1>');
+});
+
+app.listen(3000);
