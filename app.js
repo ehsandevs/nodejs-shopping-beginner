a71fc6533adbd791bc0ee6f236fe3c8382672cd4
Amir.E Haji Hussaini <niamileo@gmail.com>
2020-01-23 16:54:34 +0330

    add 404 and path filtering

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ const shopRoutes = require('./routes/shop');
 
 // Parse incoming text from forms
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(adminRoutes);
-app.use(shopRoutes)
+
+// /admin is the path filtering, so every route in adminRouter, starts with /admin/...
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found!</h1>');
+});
 
 // for running server on localhost:3000
 app.listen(3000);
