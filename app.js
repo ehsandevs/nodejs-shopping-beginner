273be6f4bb22559ee39022994bc3df7122272af7
Amir.E Haji Hussaini <niamileo@gmail.com>
2020-01-23 12:22:10 +0330

    add some comments for human readability

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// Parse incoming text from forms
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"></input><button type="submit">Add Product</button></form>');
 });
 
-app.use('/product', (req, res, next) => {
+// only for POST methods
+app.post('/product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
@@ -18,4 +20,5 @@ app.use('/', (req, res, next) => {
     res.send('<h1>Hello from Express.js</h1>');
 });
 
+// for running server on localhost:3000
 app.listen(3000);
