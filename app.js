3d40604aae3079ff6bfc282cee243501ceb29b09
Amir.E Haji Hussaini <niamileo@gmail.com>
2020-01-26 20:02:17 +0330

    cleaning up jukns

--- app.js
+++ app.js
@@ -2,24 +2,11 @@
 const express = require('express');
 // Importing body-parser, for parse incoming text from form fields
 const bodyParser = require('body-parser');
-// Handlebars is not like pug; It should be imported
-// const expressHBS = require('express-handlebars');
 //  Importing path to help give the path
 //  Because Nodejs counts path from root dir of pc not the project
 const path = require('path');
 // Running the Express app by adding Paranthasis: pexress()
 const app = express();
-
-// Usings pub view engine, and telling it to look for views in views directory
-// app.set('view engine', 'pug');
-// app.set('views', 'views');
-
-// Usings handlebars view engine, and telling it to look for views in views directory
-// hbs will be view extension
-// app.engine('hbs', expressHBS());
-// app.engine("hbs", expressHBS({ layoutsDir: "views/layouts/", defaultLayout: "main-layout", extname: "hbs" }));
-// app.set('view engine', 'hbs');
-// app.set('views', 'views');
 
 // Usings EJS view engine, and telling it to look for views in views directory
 app.set('view engine', 'ejs');
