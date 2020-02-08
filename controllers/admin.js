0e16bcd26d69804c977acec8753f5c93ddab96c3
Amir.E Haji Hussaini <niamileo@gmail.com>
2020-02-08 10:54:48 +0330

    populating form with the product

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,13 +2,11 @@ const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     // rendering the view and send page title
-    // " path: '/admin/add-product' " is for cheking and set active class to menu item
-    res.render('admin/add-product', {
+    // " path: '/admin/edit-product' " is for cheking and set active class to menu item
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        productCSS: true,
-        formCSS: true,
-        activeShop: true
+        editing: false
     });
 };
 
@@ -21,6 +19,27 @@ exports.postAddProduct = (req, res, next) => {
     const product = new Product(title, imageUrl, price, description);
     product.save();
     res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+        res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        if (!product) {
+            return res.redirect('/');
+        }
+        // rendering the view and send page title
+        // " path: '/admin/edit-product' " is for cheking and set active class to menu item
+        res.render('admin/edit-product', {
+            pageTitle: 'edit Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        });
+    });
 };
 
 exports.getProducts = (req, res, next) => {
