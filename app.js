5dc13eb98ca35e0026f6d6ecf9ab00dff54c9c83
Amir.E Haji Hussaini <niamileo@gmail.com>
2020-02-22 18:07:04 +0330

    add order and order-item model and associations

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 // Running the Express app by adding Paranthasis: pexress()
 const app = express();
@@ -53,6 +55,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User); // Optional
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem }); // Optional
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 // sequelize.sync({ force: true })
 sequelize.sync()
