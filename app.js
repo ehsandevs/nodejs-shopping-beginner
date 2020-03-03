120ac1d1e90fa6875ec1dd0dfa6d3e05bbb6817a
Amir.E Haji Hussaini <niamileo@gmail.com>
2020-03-03 11:28:48 +0330

    installing and implementing express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 // Importing body-parser, for parse incoming text from form fields
 const bodyParser = require('body-parser');
+const session = require('express-session');
 //  Importing path to help give the path
 //  Because Nodejs counts path from root dir of pc not the project
 const path = require('path');
@@ -32,6 +33,8 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 // making public directory accesible for static fils like css, images, etc.
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false }));
 
 // This is topper from other Routes, which means, all routes can use it
 app.use((req, res, next) => {
