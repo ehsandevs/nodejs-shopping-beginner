37866b7a19eb33701bc3c9fa0e8be5fda47d716b
Amir.E Haji Hussaini <niamileo@gmail.com>
2020-04-21 09:27:00 +0430

    serving images statically

--- app.js
+++ app.js
@@ -66,6 +66,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
 // making public directory accesible for static fils like css, images, etc.
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use(session({
     secret: 'my secret',
