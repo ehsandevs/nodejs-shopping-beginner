a261504f2f7255db67560489333016a311fd5efe
Amir.E Haji Hussaini <niamileo@gmail.com>
2020-04-22 12:48:32 +0430

    manipulating the dom and deleting on the fly

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,14 +2,24 @@ const deleteProduct = (btn) => {
     const prodId = btn.parentNode.querySelector('[name=productId]').value;
     const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
 
+    const productElement = btn.closest('article');
+
     fetch('/admin/product/' + prodId, {
-        method: 'DELETE',
-        headers: {
-            'csrf-token': csrf
-        }
-    }).then(result => {
-        console.log(result);
-    }).catch(err => {
-        console.log(err);
-    });
+            method: 'DELETE',
+            headers: {
+                'csrf-token': csrf
+            }
+        })
+        .then(result => {
+            return result.json();
+        })
+        .then(data => {
+            console.log(data);
+            // Works in all browsers, including IE
+            // but productElement.remove() works on modern browsers, excluding IE
+            productElement.parentNode.removeChild(productElement);
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
